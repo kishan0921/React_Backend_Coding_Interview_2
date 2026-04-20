b60adacd8b3b7dba2a67158bd158757dbb3199d8
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2026-04-20 21:30:22 +0000

    7:18:00 completed

--- JS_Backend_code_and chai/04 Project_01 revise/src/models/15 video.model.js
+++ JS_Backend_code_and chai/04 Project_01 revise/src/models/15 video.model.js
@@ -1,6 +1,12 @@
+// STEP : 01
 import mongoose , {Schema} from "mongoose";
+
+// STEP : 04 Hum ek special package ka use krenge.
+// ye package aapko allowed krta hai, "aggregation queries likhne me "
+// documentation : https://www.npmjs.com/package/mongoose-aggregate-paginate-v2
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+// STEP : 03
 const videoSchema = new Schema({
 
     // VideoField field hai
@@ -38,6 +44,8 @@ timestamps: true
 })
 
 
+// STEP : 05  - END
+//Aap export kr rahe ho "videoSchema" ko usse pehle, issko use krna hota h.
 //Monngose ek aur option deta hai, 
 //ki aap apne khud ke plugin add kr skte hai
 // and iss plugin ke saath hum mongooseAggregatePaginate  ko add krte hai.
@@ -45,4 +53,11 @@ timestamps: true
 // and jo hai na mongoose ka 'AggregatePaginate' yahi humhare project ko advanced level tk le jaayega.
 videoSchema.plugin(mongooseAggregatePaginate)
 
+
+// STEP : 02
+// Ab export kr dete hai iss schema ko, and important "Video" ye aise hi likhna hai
+//Kyuki, "Video" as reference pass kiya gaya hai.
+
+// then, mongoose ko bolenge, ek model bana kar dena, model ka name "Video" rakhenge.
+// and ye model based hoga mera videoSchema pe based.
 export const Video = mongoose.model("Video", videoSchema)
